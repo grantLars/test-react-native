90b1fad2be05d7594c1319a18d0b7301a2c3327c
Grant Larson <glars4221@live.com>
2018-06-03 22:00:12 -0700

    start react-native project

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name} !</Text>
+    )
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+
+    return (
+      <View style={styles.container}>
+        <Text>test</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Greeting name='Rexxar'/>        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
