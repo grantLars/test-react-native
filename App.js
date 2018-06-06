7a2c5a395c4702dbc741f598aa346bab1516ce6d
Grant Larson <glars4221@live.com>
2018-06-05 20:45:51 -0700

    Test styling out

--- App.js
+++ App.js
@@ -5,7 +5,94 @@ class Greeting extends Component {
   render() {
     return (
       <Text>Hello {this.props.name} !</Text>
-    )
+    );
+  }
+}
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+class First extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width:50, height: 50, backgroundColor: 'red' }} />
+        <View style={{width:50, height: 50, backgroundColor: 'green' }} />
+        <View style={{width:50, height: 50, backgroundColor: 'blue' }} />        
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+        <Text style={styles.red}>test</Text>
+        <Text style={styles.bigblue}>test2</Text>
+        <Text style={[styles.bigblue, styles.red]}>test3</Text>
+        <Text style={[styles.red, styles.bigblue]}>test4</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Greeting name='Rexxar'/>
+        <Blink text="BLINKING" />
+      </View>
+    );
+  }
+}
+
+class FlexDirection extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width:50, height: 50, backgroundColor: 'red' }} />
+        <View style={{width:50, height: 50, backgroundColor: 'green' }} />
+        <View style={{width:50, height: 50, backgroundColor: 'blue' }} />
+      </View>
+    );
+  }
+}
+
+class JustifyContent extends Component {
+  render() {
+    return(
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'yellow'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'black'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'yellow'}} />
+      </View>
+    );
+  }
+}
+
+class AlignItems extends Component {
+  render() {
+    return(
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',        
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'yellow'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'black'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'yellow'}} />
+      </View>
+    );
   }
 }
 
@@ -16,20 +103,20 @@ export default class App extends React.Component {
     };
 
     return (
-      <View style={styles.container}>
-        <Text>test</Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Greeting name='Rexxar'/>        
-      </View>
+      // <FlexDirection />
+      // <JustifyContent />
+      <AlignItems />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30
   },
+  red: {
+    color: 'red'
+  }
 });
